a6df18f7365ba7ca092eae3a96bd7cfc6088c284
Mik <mik@MacBook-Pro-von-Mik.local>
2025-03-14 22:35:48 +0100

    .

--- apps/web/middleware.ts
+++ apps/web/middleware.ts
@@ -78,8 +78,8 @@ export default async function middleware(req: NextRequest) {
         // domain: window.location.hostname // LEARNHOUSE_TOP_DOMAIN == 'localhost' ? '' : LEARNHOUSE_TOP_DOMAIN,
         // TODO: this is also completely fucked rn
         // domain: '.localhost'
-        // domain: `.${LEARNHOUSE_TOP_DOMAIN()}`
-        domain: undefined
+        domain: `.${LEARNHOUSE_TOP_DOMAIN()}`
+        // domain: undefined
       })
     }
     return response
