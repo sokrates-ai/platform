fe1f55b8012dec600f6236bb2fc4031726b8523c
Mik Müller <131623486+tyronfranzke@users.noreply.github.com>
2025-04-13 19:53:57 +0200

    chore: Remove linting errors in last commit

--- apps/web/app/orgs/[orgslug]/dash/exercises/client.tsx
+++ apps/web/app/orgs/[orgslug]/dash/exercises/client.tsx
@@ -3,7 +3,7 @@ import BreadCrumbs from '@components/Dashboard/Misc/BreadCrumbs'
 import AuthenticatedClientElement from '@components/Security/AuthenticatedClientElement'
 import Modal from '@components/Objects/StyledElements/Modal/Modal'
 import { useSearchParams } from 'next/navigation'
-import React, { useEffect } from 'react'
+import React from 'react'
 import useAdminStatus from '@components/Hooks/useAdminStatus'
 import { useLHSession } from '@components/Contexts/LHSessionContext'
 import { useOrg } from '@components/Contexts/OrgContext'
@@ -12,7 +12,6 @@ import { getAPIUrl } from '@services/config/config'
 import { swrFetcher } from '@services/utils/ts/requests'
 import CreateExerciseModal from '@components/Objects/Modals/Exercise/Create/CreateExercise'
 import ExerciseThumbnail from '@components/Objects/Thumbnails/ExerciseThumbnail'
-//import { Divide } from 'lucide-react'
 
 type ExerciseProps = {
   orgslug: string
@@ -24,10 +23,7 @@ function ExerciseHome(params: ExerciseProps) {
 
   const searchParams = useSearchParams()
   const isCreatingExercise = searchParams.get('new') ? true : false
-  // TODO: rename this in create-exercise!
   const [newExerciseModal, setNewExerciseModal] = React.useState(isCreatingExercise)
-  // const orgslug = params.orgslug
-  // const courses = params.courses
   const isUserAdmin = useAdminStatus() as any
 
   async function closeNewCourseModal() {
@@ -37,15 +33,9 @@ function ExerciseHome(params: ExerciseProps) {
   const session = useLHSession() as any;
   const access_token = session?.data?.tokens?.access_token;
   const org = useOrg() as any;
-  // const [exercises, setExercises] = React.useState<any[]>([])
 
   // TODO: set limit?
   const { data: exercises } = useSWR(TASKS_URL, (url: string) => swrFetcher(url, access_token))
-
-  // useEffect(() => {
-  //   if (res.data)
-  //     setExercises(res.data)
-  // }, [res])
 
   return (
     <div className="h-full w-full bg-[#f8f8f8] pl-10 pr-10">
