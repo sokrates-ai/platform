68ef3f929a89d95f67e29cb1d77541def09b801d
Mik Müller <83001409+MikMuellerDev@users.noreply.github.com>
2025-04-30 12:41:27 +0200

    fix: Linter issues

--- apps/web/components/Dashboard/Pages/Course/EditCourseMap/EditCourseMap.tsx
+++ apps/web/components/Dashboard/Pages/Course/EditCourseMap/EditCourseMap.tsx
@@ -9,7 +9,7 @@ import { BarLoader } from 'react-spinners';
 
 import dynamic from 'next/dynamic';
 import { AssetData } from '@components/Objects/ContentMap/Asset';
-import { SPRITES} from './spriteIndex'
+import { SPRITES} from '@components/Objects/Dashboard/Pages/Course/EditCourseMap/spriteIndex';
 import ChapterActivities from '@components/Pages/Courses/ChapterActivities';
 import { SPRITE_SCALE_FACTOR } from '@components/Objects/ContentMap/constants';
 import { setLazyProp } from 'next/dist/server/api-utils';
