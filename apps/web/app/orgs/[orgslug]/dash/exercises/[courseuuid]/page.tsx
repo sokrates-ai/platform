74ceb4a8174bb8adac8b66ade87d7986fbe1b887
Mik Müller <83001409+MikMuellerDev@users.noreply.github.com>
2025-06-23 12:26:03 +0200

    feat: Allow viewing unassigned exercises

--- apps/web/app/orgs/[orgslug]/dash/exercises/[courseuuid]/page.tsx
+++ apps/web/app/orgs/[orgslug]/dash/exercises/[courseuuid]/page.tsx
@@ -18,159 +18,183 @@ import CourseListing from '../courseListing'
 import { useRouter } from 'next/navigation'
 
 type ExerciseProps = {
-    params: {
-        orgslug: string,
-        courseuuid: string,
-    },
+  params: {
+    orgslug: string
+    courseuuid: string
+  }
 }
 
 function ExerciseCourseHome(params: ExerciseProps) {
-    const tasks_page = 1;
-    const tasks_limit = 100;
-    const TASKS_URL = `${getAPIUrl()}tasks/list/page/${tasks_page}/limit/${tasks_limit}`;
+  const tasks_page = 1
+  const tasks_limit = 100
+  const TASKS_URL = `${getAPIUrl()}tasks/list/page/${tasks_page}/limit/${tasks_limit}`
 
-    const course_page = 1;
-    const course_limit = 100;
-    const COURSES_URL = `${getAPIUrl()}courses/org_slug/${(params as any).params.orgslug}/page/${course_page}/limit/${course_limit}`
+  const course_page = 1
+  const course_limit = 100
+  const COURSES_URL = `${getAPIUrl()}courses/org_slug/${
+    (params as any).params.orgslug
+  }/page/${course_page}/limit/${course_limit}`
 
-    const TAGS_URL = `${getAPIUrl()}tasks/tag`
+  const TAGS_URL = `${getAPIUrl()}tasks/tag`
 
-    const searchParams = useSearchParams()
-    const isCreatingExercise = searchParams.get('new') ? true : false
-    const [newExerciseModal, setNewExerciseModal] = React.useState(isCreatingExercise)
-    const [editTagsModalOpen, setEditTagsModalOpen] = React.useState(false)
-    const isUserAdmin = useAdminStatus() as any
+  const searchParams = useSearchParams()
+  const isCreatingExercise = searchParams.get('new') ? true : false
+  const [newExerciseModal, setNewExerciseModal] =
+    React.useState(isCreatingExercise)
+  const [editTagsModalOpen, setEditTagsModalOpen] = React.useState(false)
+  const isUserAdmin = useAdminStatus() as any
 
-    async function closeNewCourseModal() {
-        setNewExerciseModal(false)
-    }
+  async function closeNewCourseModal() {
+    setNewExerciseModal(false)
+  }
 
-    const session = useLHSession() as any;
-    const access_token = session?.data?.tokens?.access_token;
-    const org = useOrg() as any;
+  const session = useLHSession() as any
+  const access_token = session?.data?.tokens?.access_token
+  const org = useOrg() as any
 
-    const { data: courses, isLoading: coursesLoading } = useSWR(COURSES_URL, (url: string) => swrFetcher(url, access_token))
+  const { data: courses, isLoading: coursesLoading } = useSWR(
+    COURSES_URL,
+    (url: string) => swrFetcher(url, access_token)
+  )
 
-    // TODO: set limit?
-    const { data: exercises, isLoading: exercisesLoading } = useSWR(TASKS_URL, (url: string) => swrFetcher(url, access_token))
+  // TODO: set limit?
+  const { data: exercises, isLoading: exercisesLoading } = useSWR(
+    TASKS_URL,
+    (url: string) => swrFetcher(url, access_token)
+  )
 
-    const { data: tags, isLoading: tagsLoading } = useSWR(TAGS_URL, (url: string) => swrFetcher(url, access_token))
+  const { data: tags, isLoading: tagsLoading } = useSWR(
+    TAGS_URL,
+    (url: string) => swrFetcher(url, access_token)
+  )
 
-    // const router = useRouter()
-    // console.log(router)
-    // const path = usePathname()
-    // const course_uuid = router.query.courseuuid as string
+  // const router = useRouter()
+  // console.log(router)
+  // const path = usePathname()
+  // const course_uuid = router.query.courseuuid as string
 
-    console.log(COURSES_URL)
-    const course_id = parseInt(params.params.courseuuid)
+  console.log(COURSES_URL)
+  const courseIDString = params.params.courseuuid
+  let course_id = 100000000
+  if (courseIDString === 'unassigned') {
+    course_id = -1
+  } else {
+    course_id = parseInt(courseIDString)
+  }
 
-    if (coursesLoading || exercisesLoading || tagsLoading) {
-        return;
-    }
+  if (coursesLoading || exercisesLoading || tagsLoading) {
+    return
+  }
 
-    console.log(courses)
-    const course = courses.find((c: any) => c.id === course_id)
-    if (!course) {
-        return;
-    }
+  const course = courses.find((c: any) => c.id === course_id)
+  if (!course && course_id != -1) {
+    return
+  }
 
-    // {(!!exercises && !!tags) ?
-    //   (<div>
-    //     {courses.map((course: any) => (
-    //       <div>
-    //         {course.name}
-    //       </div>
-    //     ))}
-    //   </div>)
-    //   : (<span>LOADING...</span>)
-    // }
+  // {(!!exercises && !!tags) ?
+  //   (<div>
+  //     {courses.map((course: any) => (
+  //       <div>
+  //         {course.name}
+  //       </div>
+  //     ))}
+  //   </div>)
+  //   : (<span>LOADING...</span>)
+  // }
 
-    return (
-        <div className="h-full w-full pl-10 pr-10">
-            <div className="mb-6">
-                <BreadCrumbs type="exercises" />
-                <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mt-4">
-                    <h1 className="text-3xl font-bold mb-4 sm:mb-0">Course Exercises: <q>{course.name}</q></h1>
-                    <AuthenticatedClientElement
-                        checkMethod="roles"
-                        action="create"
-                        ressourceType="courses"
-                        orgId={org.id}
-                    >
-                        <div className="flex gap-5">
-                            <Modal
-                                isDialogOpen={editTagsModalOpen}
-                                onOpenChange={setEditTagsModalOpen}
-                                minHeight="md"
-                                dialogContent={
-                                    <EditTagsModal
-                                        closeModal={() => setEditTagsModalOpen(false)}
-                                        orgslug={params.params.orgslug}
-                                        mutateURL={TAGS_URL}
-                                        tags={tags}
-                                    />
-                                }
-                                dialogTitle="Edit Tags"
-                                dialogDescription="Edit task tags"
-                                dialogTrigger={
-                                    <button>
-                                        <button className="rounded-lg bg-black hover:scale-105 transition-all duration-100 ease-linear antialiased ring-offset-purple-800 p-2 px-5 my-auto font text-xs font-bold text-white drop-shadow-lg flex space-x-2 items-center">
-                                            <div>Edit Tags</div>
-                                            {/* <div className="text-md bg-neutral-800 px-1 rounded-full">+</div> */}
-                                        </button>
-                                    </button>
-                                }
-                            />
+  return (
+    <div className="h-full w-full pl-10 pr-10">
+      <div className="mb-6">
+        <BreadCrumbs type="exercises" />
+        <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center mt-4">
+          <h1 className="text-3xl font-bold mb-4 sm:mb-0">
+            {course ? (
+              <>
+                Course Exercises: <q>{course.name}</q>
+              </>
+            ) : (
+              <>Unassigned Exercises</>
+            )}
+          </h1>
+          <AuthenticatedClientElement
+            checkMethod="roles"
+            action="create"
+            ressourceType="courses"
+            orgId={org.id}
+          >
+            <div className="flex gap-5">
+              <Modal
+                isDialogOpen={editTagsModalOpen}
+                onOpenChange={setEditTagsModalOpen}
+                minHeight="md"
+                dialogContent={
+                  <EditTagsModal
+                    closeModal={() => setEditTagsModalOpen(false)}
+                    orgslug={params.params.orgslug}
+                    mutateURL={TAGS_URL}
+                    tags={tags}
+                  />
+                }
+                dialogTitle="Edit Tags"
+                dialogDescription="Edit task tags"
+                dialogTrigger={
+                  <button>
+                    <button className="rounded-lg bg-black hover:scale-105 transition-all duration-100 ease-linear antialiased ring-offset-purple-800 p-2 px-5 my-auto font text-xs font-bold text-white drop-shadow-lg flex space-x-2 items-center">
+                      <div>Edit Tags</div>
+                      {/* <div className="text-md bg-neutral-800 px-1 rounded-full">+</div> */}
+                    </button>
+                  </button>
+                }
+              />
 
-                            <Modal
-                                isDialogOpen={newExerciseModal}
-                                onOpenChange={setNewExerciseModal}
-                                minHeight="md"
-                                dialogContent={
-                                    <CreateExerciseModal
-                                        closeModal={closeNewCourseModal}
-                                        orgslug={params.params.orgslug}
-                                        mutateURL={TASKS_URL}
-                                        courses={courses}
-                                        tags={tags}
-                                        courseID={course_id}
-                                    />
-                                }
-                                dialogTitle="Create Exercise"
-                                dialogDescription="Create a new exercise"
-                                dialogTrigger={
-                                    <button>
-                                        <button className="rounded-lg bg-black hover:scale-105 transition-all duration-100 ease-linear antialiased ring-offset-purple-800 p-2 px-5 my-auto font text-xs font-bold text-white drop-shadow-lg flex space-x-2 items-center">
-                                            <div>New Exercise</div>
-                                            <div className="text-md bg-neutral-800 px-1 rounded-full">+</div>
-                                        </button>
-                                    </button>
-                                }
-                            />
-                        </div>
-                    </AuthenticatedClientElement>
-                </div>
+              <Modal
+                isDialogOpen={newExerciseModal}
+                onOpenChange={setNewExerciseModal}
+                minHeight="md"
+                dialogContent={
+                  <CreateExerciseModal
+                    closeModal={closeNewCourseModal}
+                    orgslug={params.params.orgslug}
+                    mutateURL={TASKS_URL}
+                    courses={courses}
+                    tags={tags}
+                    courseID={course_id}
+                  />
+                }
+                dialogTitle="Create Exercise"
+                dialogDescription="Create a new exercise"
+                dialogTrigger={
+                  <button>
+                    <button className="rounded-lg bg-black hover:scale-105 transition-all duration-100 ease-linear antialiased ring-offset-purple-800 p-2 px-5 my-auto font text-xs font-bold text-white drop-shadow-lg flex space-x-2 items-center">
+                      <div>New Exercise</div>
+                      <div className="text-md bg-neutral-800 px-1 rounded-full">
+                        +
+                      </div>
+                    </button>
+                  </button>
+                }
+              />
             </div>
-
-
-            {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"> */}
-            <CourseListing
-                isUserAdmin={isUserAdmin}
-                exercises={exercises}
-                tags={tags}
-                courses={courses}
-                org={org}
-                orgslug={params.params.orgslug}
-                TASKS_URL={TASKS_URL}
-                COURSES_URL={COURSES_URL}
-                TAGS_URL={TAGS_URL}
-                course_id={course_id}
-            >
-
-            </CourseListing>
-            {/* </div> */}
+          </AuthenticatedClientElement>
         </div>
-    )
+      </div>
+
+      {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4"> */}
+      <CourseListing
+        isUserAdmin={isUserAdmin}
+        exercises={exercises}
+        tags={tags}
+        courses={courses}
+        org={org}
+        orgslug={params.params.orgslug}
+        TASKS_URL={TASKS_URL}
+        COURSES_URL={COURSES_URL}
+        TAGS_URL={TAGS_URL}
+        course_id={course_id}
+      ></CourseListing>
+      {/* </div> */}
+    </div>
+  )
 }
 
 export default ExerciseCourseHome
--- apps/web/app/orgs/[orgslug]/dash/exercises/client.tsx
+++ apps/web/app/orgs/[orgslug]/dash/exercises/client.tsx
@@ -143,7 +143,6 @@ function ExerciseHome(params: ExerciseProps) {
             ? getCourseThumbnailMediaDirectory(org?.org_uuid, course.course_uuid, course.thumbnail_image)
             : '../empty_thumbnail.png'
 
-          console.log("COURSE_IMAGE_URL", thumbnailImage)
           return (<div key={course.course_uuid}>
             <CourseCard
               title={course.name}
@@ -155,6 +154,16 @@ function ExerciseHome(params: ExerciseProps) {
           </div>
           )
         })}
+
+          <div key={'unassigned'}>
+            <CourseCard
+              title={"Unassigned"}
+              description={"Exercises without a course"}
+              imageUrl={undefined}
+              onClick={() => { window.location.href = `/dash/exercises/unassigned` }}
+            >
+            </CourseCard>
+          </div>
       </div>
     </div>
   )
